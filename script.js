6eab18d44530a178355fc921c1605cb98a7bbce5
Roman Meshkov <meshkov.ra@yandex.ru>
2018-03-18 19:24:56 +0300

    added navigation block while scrolling

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-document.addEventListener('DOMContentLoaded',onload);
-
-
-function onload() {
-
-}
+window.onscroll = function (ev) {
+  if(screen.width>=769){
+      if(window.pageYOffset >= document.getElementById('carouselExampleIndicators').offsetTop){
+          document.getElementById('nav_block_onscroll').style.display = 'block';
+      }
+      else{
+          document.getElementById('nav_block_onscroll').style.display = 'none';
+      }
+  }
+  else return 0;
+};
